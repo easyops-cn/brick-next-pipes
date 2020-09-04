5deee2741afc6e7f1c50194d3e27dc114683b872
stevewang <stevewang@easyops.cn>
2020-09-04 16:30:30 +0800

    docs: build docs using api-extractor

--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,41 +1,69 @@
-export { add } from "./add";
-export { boolean, boolean as bool } from "./boolean";
-export { cmdbInstanceShowName } from "./cmdbInstanceShowName";
-export { countBy } from "./countBy";
-export { datetime } from "./datetime";
-export { deltaTime } from "./deltaTime";
-export { divide } from "./divide";
-export { equal } from "./equal";
-export { find } from "./find";
-export { findIndex } from "./findIndex";
-export { findLast } from "./findLast";
-export { findLastIndex } from "./findLastIndex";
-export { get } from "./get";
-export { graphTree } from "./graphTree";
-export { groupBy } from "./groupBy";
-export { groupByToIndex } from "./groupByToIndex";
-export { includes } from "./includes";
-export { join } from "./join";
-export { json } from "./json";
-export { jsonStringify } from "./jsonStringify";
-export { keyBy } from "./keyBy";
-export { map } from "./map";
-export { mapToArray } from "./mapToArray";
-export { multiply } from "./multiply";
-export { not } from "./not";
-export { nullish } from "./nullish";
-export { number } from "./number";
-export { parseTimeRange } from "./parseTimeRange";
-export { reverse } from "./reverse";
-export { slice } from "./slice";
-export { sort } from "./sort";
-export { split } from "./split";
-export { string } from "./string";
-export { substr } from "./substr";
-export { substring } from "./substring";
-export { subtract } from "./subtract";
-export { ternary } from "./ternary";
-export { uniq } from "./uniq";
-export { unitFormat } from "./unitFormat";
-export { yaml } from "./yaml";
-export { yamlStringify } from "./yamlStringify";
+/**
+ * A library of pipes for Brick Next.
+ *
+ * @remarks
+ *
+ * It also includes these functions transparently exported from lodash:
+ *
+ * equal (from {@link https://lodash.com/docs/#isEqual | _.isEqual})
+ *
+ * {@link https://lodash.com/docs/#find | find}
+ *
+ * {@link https://lodash.com/docs/#findIndex | findIndex}
+ *
+ * {@link https://lodash.com/docs/#findLast | findLast}
+ *
+ * {@link https://lodash.com/docs/#findLastIndex | findLastIndex}
+ *
+ * {@link https://lodash.com/docs/#get | get}
+ *
+ * {@link https://lodash.com/docs/#groupBy | groupBy}
+ *
+ * {@link https://lodash.com/docs/#keyBy | keyBy}
+ *
+ * {@link https://lodash.com/docs/#uniq | uniq}
+ *
+ *
+ * @packageDocumentation
+ */
+export * from "./add";
+export * from "./boolean";
+export * from "./cmdbInstanceShowName";
+export * from "./countBy";
+export * from "./datetime";
+export * from "./deltaTime";
+export * from "./divide";
+export * from "./equal";
+export * from "./find";
+export * from "./findIndex";
+export * from "./findLast";
+export * from "./findLastIndex";
+export * from "./get";
+export * from "./graphTree";
+export * from "./groupBy";
+export * from "./groupByToIndex";
+export * from "./includes";
+export * from "./join";
+export * from "./json";
+export * from "./jsonStringify";
+export * from "./keyBy";
+export * from "./map";
+export * from "./mapToArray";
+export * from "./multiply";
+export * from "./not";
+export * from "./nullish";
+export * from "./number";
+export * from "./parseTimeRange";
+export * from "./reverse";
+export * from "./slice";
+export * from "./sort";
+export * from "./split";
+export * from "./string";
+export * from "./substr";
+export * from "./substring";
+export * from "./subtract";
+export * from "./ternary";
+export * from "./uniq";
+export * from "./unitFormat";
+export * from "./yaml";
+export * from "./yamlStringify";
