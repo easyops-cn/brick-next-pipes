29c8df5322fd10074102a99c0a1079450320e769
MJmajiong <936233505@qq.com>
2024-09-25 16:28:56 +0800

    feat: 增加支持中文的base64转换

--- src/pipes/btoaUnicode.spec.ts
+++ src/pipes/btoaUnicode.spec.ts
@@ -3,6 +3,7 @@ import { btoaUnicode } from "./btoaUnicode";
 describe("btoaUnicode", () => {
   const testCases: [string, string][] = [
     ["你好, 世界", "5L2g5aW9LCDkuJbnlYw="],
+    ["中文编码", "5Lit5paH57yW56CB"],
   ];
   test.each(testCases)("btoaUnicode(%j) should return %j", (input, output) => {
     expect(btoaUnicode(input)).toEqual(output);
