234aaa20fb6fb5109f5c0d292d1889ad616ae2e9
abert <abertluo@easyops.cn>
2025-03-21 10:08:19 +0800

    fix: 调整文案 Closes CMDB_INSTANCE-2615

--- src/pipes/cmdbInstanceShowName.ts
+++ src/pipes/cmdbInstanceShowName.ts
@@ -30,11 +30,11 @@
  */
 export function cmdbInstanceShowName(value: string | string[]): string {
   if (Array.isArray(value)) {
-    const firstKey = value[0] || ""; // 第一个show key为undefined时 且 第二个也为undefined 显示"-"
+    const firstKey = value[0] || ""; // 第一个show key为undefined时 且 第二个也为undefined 显示 "-"
     const resKey = value.slice(1, value.length).join(",");
     const res = resKey
       ? `${firstKey}${firstKey ? `(${resKey})` : `${resKey}`}`
-      : firstKey || "-";
+      : firstKey || "-"; // 第一个show key为undefined时 且 第二个为有值 显示 "第二个值不需要括号"
     return res;
   } else {
     return value;
